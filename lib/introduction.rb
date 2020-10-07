33bd05e9798d4a8be02be72feeab9e41fb03f71e
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-07 20:53:04 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+# Define a method that takes in an argument and uses that argument in the method body.
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
