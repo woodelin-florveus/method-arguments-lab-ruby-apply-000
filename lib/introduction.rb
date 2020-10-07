4e71f264f64e8d1bf63554acece9174f803256af
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-07 20:52:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
 # Define a method that takes in an argument and uses that argument in the method body.
 
 def introduction(name)
-  puts "hello my name is #{name.strip}"
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
