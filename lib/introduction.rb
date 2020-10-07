52ae17ee4f3c597b247f52b96f657c56a256b421
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-07 20:51:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,6 @@ def introduction(name)
   puts "hello my name is #{name.strip}"
 end
 
-def introduction_with_language(name,language)
+def introduction_with_language(name, language)
   puts "hello my name is #{name} and i speak, #{language}"
 end
