ea65ae636becce9a3f36f0118a07c605a7653f9f
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-07 20:47:38 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,9 @@
 # Define a method that takes in an argument and uses that argument in the method body.
 
 def something(argument)
-  return "#{argument}"
+  puts "hello this is an example of a #{argument}"
+end
+
+def something(argument_one, argument_two)
+  puts "hello this is an example of a #{argument_one} and #{argument_two}"
 end
